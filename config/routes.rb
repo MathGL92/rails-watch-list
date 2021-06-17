d6925f20127f431748bd599795238e3797b40a5e
Mathieu Longe <mathieu.longe@orange.fr>
2021-06-17 17:33:14 +1000

    as a user I can see all lists, one list, create a new list

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'lists', to: 'lists#index'
+  get 'list/new', to: 'lists#new', as: :list_new
+  post 'lists', to: 'lists#create'
+  get 'list/:id', to: 'lists#show', as: :list
 end
