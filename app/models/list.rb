9363676ec4ec5444117ec5c6247f9bd7ac05f43d
Mathieu Longe <mathieu.longe@orange.fr>
2021-06-17 16:37:08 +1000

    list tests passing

---
+++ app/models/list.rb
@@ -0,0 +1,8 @@
+class List < ApplicationRecord
+  has_many :bookmarks, dependent: :destroy
+  has_many :movies, through: :bookmarks, dependent: :destroy
+
+  validates_uniqueness_of :name
+  validates_presence_of :name
+
+end
